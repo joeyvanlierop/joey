4868ef3716f4f7884bef47c3cc61955f7ed06ac8
Joey Van Lierop <joey.vanlierop@me.com>
2021-06-04 21:23:42 -0600

    fix: enlargen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1 className="text-sm">Hi, I'm Joey.</h1>
+      <h1 className="text-9xl">Hi, I'm Joey.</h1>
     </div>
   );
 }
