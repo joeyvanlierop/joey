4c7e938f582e933d03d53086e87b99a1f1bd6128
Joey Van Lierop <joey.vanlierop@me.com>
2021-09-09 15:29:36 -0600

    deletus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,7 @@
-import { BiGame, BiMovie, BiNotepad } from "react-icons/bi";
 import styled from "styled-components";
-import { About } from "../components/About";
-import { Bubble } from "../components/Bubble";
 
 export default function Home() {
-  return (
-    <>
-      {/* About */}
-      <Hero>
-        <About
-          name="joey van lierop"
-          lines={["random text", "more text blah blah"]}
-        />
-      </Hero>
-      {/* Writing */}
-      <Bubble
-        color="#fdebf1"
-        innerSize="5rem"
-        outerSize="50rem"
-        offset={1}
-        href="/writing"
-        top="30%"
-        left="30%"
-        transitions={["#fdebf1", "#fdf3f6", "#fff9fb", "#ffffff"]}
-      >
-        <BiNotepad fontSize="2rem" color="#8a5768" />
-      </Bubble>
-      {/* Games */}
-      <Bubble
-        color="#e5f1f8"
-        innerSize="5rem"
-        outerSize="40rem"
-        offset={3}
-        href="/games"
-        top="40%"
-        left="70%"
-        transitions={["#e5f1f8", "#edf5fa", "#f5fafd", "#ffffff"]}
-      >
-        <BiGame fontSize="2rem" color="#61869b" />
-      </Bubble>
-      {/* Movies */}
-      <Bubble
-        color="#feecea"
-        innerSize="5rem"
-        outerSize="30rem"
-        offset={2}
-        href="/movies"
-        top="65%"
-        left="60%"
-        transitions={["#feecea", "#fff2f6", "#fff9fb", "#ffffff"]}
-      >
-        <BiMovie fontSize="2rem" color="#995952" />
-      </Bubble>
-    </>
-  );
+  return <Hero>i'll update this eventually</Hero>;
 }
 
 const Hero = styled.div`
