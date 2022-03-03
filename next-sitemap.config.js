cd8b701409acff45b9e628c06217c9edf31abd48
Joey Van Lierop <joey.vanlierop@me.com>
2022-03-03 16:45:47 -0700

    change siteurl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || "https://www.joeyvanlierop.com",
+  siteUrl:
+    process.env.NEXT_PUBLIC_VERCEL_URL || "https://www.joeyvanlierop.com",
   generateRobotsTxt: true,
 };
