26358e79d7c4870ad4c3c33ad7338a501e5067df
Joey Van Lierop <joey.vanlierop@me.com>
2023-08-09 16:44:52 -0600

    Git stuff in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,10 @@ export function Footer() {
     <footer className="h-12 w-full border-t border-[#2e2e2e] absolute bottom-0 flex justify-center items-center text-sm text-[#707070]">
       <Column className="justify-center">
         <div className="flex justify-between items-center">
-          <span>I am the footer</span>
+          <span>
+            {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}:{" "}
+            {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE}
+          </span>
           <Clock />
         </div>
       </Column>
