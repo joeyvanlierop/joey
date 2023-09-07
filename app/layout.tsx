21b16c39bb7c60230c966f1523a402b118023299
Joey Van Lierop <joey.vanlierop@me.com>
2023-09-07 00:17:52 -0600

    Adding analytics (for now)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 
 import { Footer } from "@components/footer";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import Providers from "./providers";
 
@@ -46,6 +47,7 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
