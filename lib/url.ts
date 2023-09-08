6e0c11d45209e918ccd3d776d3ef08a2e8ff5e0d
Joey Van Lierop <joey.vanlierop@me.com>
2023-09-07 20:29:41 -0600

    Unbreak sitemap

--- lib/url.ts
+++ lib/url.ts
@@ -1,5 +1,5 @@
 export const getUrl = () => {
-  return process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "https://www.joeyvanlierop.com";
+  return process.env.VERCEL_ENV === "production"
+    ? "https://www.joeyvanlierop.com"
+    : `https://${process.env.VERCEL_URL}`;
 };
