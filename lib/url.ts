a3fe01a3bcff3ca31f315850b32e6048cf30993c
Joey Van Lierop <joey.vanlierop@me.com>
2023-09-10 23:22:03 -0600

    Actually update sitemap

--- lib/url.ts
+++ lib/url.ts
@@ -1,6 +1,6 @@
 export const getUrl = () => {
   if (process.env.VERCEL_ENV === undefined) return "localhost:3000";
   if (process.env.VERCEL_ENV === "production")
-    return "https://www.joeyvanlierop.com";
+    return "https://joeyvanlierop.com";
   return `https://${process.env.VERCEL_URL}`;
 };
