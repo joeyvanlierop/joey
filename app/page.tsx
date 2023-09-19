34e0564638c3d47514dd3dca24744e192b46360e
Joey Van Lierop <joey.vanlierop@me.com>
2023-09-19 16:59:00 -0600

    Rename post

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default async function Home() {
           <section className="flex mb-8 -mx-10 pl-10 gap-8 animate-enter animate-delay-[360ms] overflow-x-auto mask">
             <ShowoffSection title="Writing">
               <WritingShowoff slug={"the-west-coast-trail"} />
-              <WritingShowoff slug={"hasta-la-vista"} />
+              <WritingShowoff slug={"ciao"} />
 
               {/* TODO: Make the dots jump */}
               <Showoff
