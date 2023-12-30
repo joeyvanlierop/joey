dfe11363b395373819dc62c69c5ab818baad57a5
Joey Van Lierop <joey.vanlierop@me.com>
2023-12-30 02:02:07 -0700

    Add mood history

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,30 @@ export function Footer() {
     <footer className="h-12 w-full border-t border-[#2e2e2e] absolute bottom-0 flex justify-center items-center text-sm text-[#707070]">
       <Column className="justify-center">
         <div className="flex justify-between items-center">
-          <Mood message="December Holidays are S-tier" />
+          <Mood
+            current={{
+              message: "December Holidays are S-tier",
+              date: "2023-12-30",
+            }}
+            history={[
+              {
+                message: "On the grind",
+                date: "2023-10-19",
+              },
+              {
+                message: "Back to the grind",
+                date: "2023-09-07",
+              },
+              {
+                message: "Hiking the West Coast Trail, back in a bit",
+                date: "2023-08-10",
+              },
+              {
+                message: "Hello world",
+                date: "2023-07-30",
+              },
+            ]}
+          />
           <Clock>
             <Wayback />
           </Clock>
--- components/mood.tsx
+++ components/mood.tsx
@@ -1,10 +1,55 @@
+"use client";
+
+import * as Tooltip from "@radix-ui/react-tooltip";
+import dayjs from "dayjs";
 import { Dot } from "./dot";
 
-export function Mood({ message }: { message: string }) {
+type MoodMeta = { message: string; date: string };
+
+export function Mood({
+  current,
+  history,
+}: {
+  current: MoodMeta;
+  history: MoodMeta[];
+}) {
   return (
-    <div className="font-title flex items-center gap-2">
-      <Dot className="bg-yellow-500 shadow-yellow-500 animate-pulse shadow-dot" />
-      <p className="text-sm -mt-0.5">{message}</p>
-    </div>
+    <Tooltip.Provider delayDuration={100} skipDelayDuration={0}>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <div className="font-title flex items-center gap-2">
+            <Dot className="bg-yellow-500 shadow-yellow-500 animate-pulse shadow-dot" />
+            <p className="text-sm -mt-0.5">{current.message}</p>
+          </div>
+        </Tooltip.Trigger>
+        <Tooltip.Content
+          className="border border-[#2e2e2e] bg-[#1a1a1a] px-3 py-2 rounded-lg data-[state=delayed-open]:animate-tooltip-in data-[state=closed]:animate-tooltip-out shadow-dark text-white tabular-nums"
+          sideOffset={10}
+          align="start"
+        >
+          <ol className="flex max-h-96 overflow-y-scroll -mr-3 flex-col-reverse">
+            {history.map((message, index) => (
+              <li
+                className="py-2 border-b first:border-b-0 border-[#2e2e2e]"
+                key={index}
+              >
+                <MoodEntry {...message} />
+              </li>
+            ))}
+          </ol>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  );
+}
+
+function MoodEntry({ message, date }: MoodMeta) {
+  return (
+    <span>
+      <time className="font-header text-[#a0a0a0] pr-2 inline-block min-w-[60px]">
+        {dayjs(date).format("MMM 'YY")}
+      </time>
+      {message}
+    </span>
   );
 }
