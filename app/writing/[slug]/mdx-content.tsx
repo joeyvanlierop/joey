5914a9f5a8c908d4a3bf5f0b234457b2d2973be3
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-01 16:25:07 -0700

    January journaling

--- app/writing/[slug]/mdx-content.tsx
+++ app/writing/[slug]/mdx-content.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { Nook } from "@components/home";
+import * as Journal from "@components/journal";
 import { MDXRemote } from "next-mdx-remote";
 
+const components = { Journal, Nook };
+
 export function MdxContent({ source }) {
-  return <MDXRemote {...source} />;
+  return <MDXRemote {...source} components={components} />;
 }
---
+++ components/journal.tsx
@@ -0,0 +1,32 @@
+import dayjs from "dayjs";
+
+function Root(props: { children: React.ReactNode }) {
+  return <div className={"flex flex-col gap-14"}>{props.children}</div>;
+}
+
+function Section(props: { date: string; children: React.ReactNode }) {
+  return (
+    <section className={"flex flex-col gap-4"}>
+      <div className="flex items-center gap-4">
+        <time className="text-[#a0a0a0] shrink-0">
+          {dayjs(props.date).format("dddd, MMMM Do")}
+        </time>
+        <div className="h-[1px] bg-[#a0a0a0] rounded-full w-full" />
+      </div>
+      {props.children}
+    </section>
+  );
+}
+
+function Entry(props: { time: string; children: React.ReactNode }) {
+  return (
+    <div className={"flex flex-col [&>p]:m-0 bg-[#242424] rounded-xl p-4"}>
+      <time className="text-[#a0a0a0] font-semibold font-header text-sm mb-3">
+        {props.time}
+      </time>
+      {props.children}
+    </div>
+  );
+}
+
+export { Root, Section, Entry };
