a9701fda57f41594686890a6e2abbc2aa9ca9d05
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-02 00:29:49 -0700

    Remove fade placeholder

--- app/writing/[slug]/mdx-content.tsx
+++ app/writing/[slug]/mdx-content.tsx
@@ -4,21 +4,14 @@ import { Nook } from "@components/home";
 import * as Journal from "@components/journal";
 import { MDXRemote } from "next-mdx-remote";
 import NextImage from "next/image";
-import { useState } from "react";
 
-const KnifeImage = () => {
-  const [blur, setBlur] = useState(true);
-
-  return (
-    <NextImage
-      src={require("@public/images/DSC06435.jpg")}
-      placeholder="blur"
-      alt="Da boys with their knives"
-      className={blur ? "blur-[10px]" : "animate-unblur"}
-      onLoadingComplete={() => setBlur(false)}
-    />
-  );
-};
+const KnifeImage = () => (
+  <NextImage
+    src={require("@public/images/DSC06435.jpg")}
+    placeholder="blur"
+    alt="Da boys with their knives"
+  />
+);
 
 const components = { Journal, Nook, KnifeImage };
 
