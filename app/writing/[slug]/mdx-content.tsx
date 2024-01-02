bcdb2c6457163d3e91e6364bf063f514407c43c6
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-01 19:18:59 -0700

    Some more tests

--- app/writing/[slug]/mdx-content.tsx
+++ app/writing/[slug]/mdx-content.tsx
@@ -4,8 +4,13 @@ import { Nook } from "@components/home";
 import * as Journal from "@components/journal";
 import { MDXRemote } from "next-mdx-remote";
 import Image from "next/image";
+import knife from "@public/images/DSC06435.jpg";
 
-const components = { Journal, Nook, Image };
+const ImageKnife = () => (
+  <Image src={knife} alt="Sunset on The West Coast Trail" />
+);
+
+const components = { Journal, Nook, ImageKnife };
 
 export function MdxContent({ source }) {
   return <MDXRemote {...source} components={components} />;
