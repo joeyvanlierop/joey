e730798f1eae4bf02b076992de004121e8122df1
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-02 00:26:38 -0700

    Fade placeholder

--- app/writing/[slug]/mdx-content.tsx
+++ app/writing/[slug]/mdx-content.tsx
@@ -4,14 +4,22 @@ import { Nook } from "@components/home";
 import * as Journal from "@components/journal";
 import { MDXRemote } from "next-mdx-remote";
 import NextImage from "next/image";
+import { useState } from "react";
 
-const KnifeImage = () => (
-  <NextImage
-    src={require("@public/images/DSC06435.jpg")}
-    placeholder="blur"
-    alt="Da boys with their knives"
-  />
-);
+const KnifeImage = () => {
+  const [blur, setBlur] = useState(true);
+
+  return (
+    <NextImage
+      src={require("@public/images/DSC06435.jpg")}
+      placeholder="blur"
+      alt="Da boys with their knives"
+      className={blur ? "blur-[20px]" : "animate-unblur"}
+      onLoadingComplete={() => setBlur(false)}
+    />
+  );
+};
+
 const components = { Journal, Nook, KnifeImage };
 
 export function MdxContent({ source }) {
