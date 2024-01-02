94aa8f3567d056f846aadd151def095824608778
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-02 12:06:49 -0700

    More image tests

--- app/writing/[slug]/mdx-content.tsx
+++ app/writing/[slug]/mdx-content.tsx
@@ -3,22 +3,14 @@
 import { Nook } from "@components/home";
 import * as Journal from "@components/journal";
 import { MDXRemote } from "next-mdx-remote";
-import NextImage from "next/image";
+import Image from "next/image";
 import { forwardRef } from "react";
-
-const KnifeImage = () => (
-  <NextImage
-    src={require("@public/images/DSC06435.jpg")}
-    placeholder="blur"
-    alt="Da boys with their knives"
-  />
-);
 
 const FancyItalics = forwardRef<HTMLElement>((props, ref) => (
   <em className="fancy" ref={ref} {...props} />
 ));
 
-const components = { Journal, Nook, KnifeImage, em: FancyItalics };
+const components = { Journal, Nook, Image, em: FancyItalics };
 
 export function MdxContent({ source }) {
   return <MDXRemote {...source} components={components} />;
