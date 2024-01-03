a9073abdbdf5e2786ae26d3a16af203c38ef0db3
Joey Van Lierop <joey.vanlierop@me.com>
2024-01-02 20:32:09 -0700

    Add another journal entry

--- components/journal.tsx
+++ components/journal.tsx
@@ -8,10 +8,10 @@ function Section(props: { date: string; children: React.ReactNode }) {
   return (
     <section className={"flex flex-col gap-4"}>
       <div className="flex items-center gap-4">
-        <time className="text-mono-11 shrink-0">
+        <time className="text-mono-11 font-header shrink-0">
           {dayjs(props.date).format("dddd, MMMM Do")}
         </time>
-        <div className="h-[1px] bg-mono-11 rounded-full w-full" />
+        <div className="h-[1px] bg-mono-5 rounded-full w-full" />
       </div>
       {props.children}
     </section>
@@ -25,7 +25,7 @@ function Entry(props: { time: string; children: React.ReactNode }) {
         "flex flex-col [&>p]:m-0 [&>img]:mt-2 [&>img]:mb-0 [&>img]:rounded-lg bg-mono-3 rounded-lg p-4"
       }
     >
-      <time className="text-mono-11 font-semibold font-header text-sm mb-3">
+      <time className="text-mono-11 font-medium font-header text-sm mb-3">
         {props.time}
       </time>
       {props.children}
