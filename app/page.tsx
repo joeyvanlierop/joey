51437900fb34f019bce938515c99425cdb2931e3
Joey Van Lierop <joey.vanlierop@me.com>
2024-02-25 01:21:42 -0700

    Showoff Surprise Pass

--- app/page.tsx
+++ app/page.tsx
@@ -53,8 +53,8 @@ export default async function Home() {
           {/* Pillars */}
           <section className="flex mb-8 -mx-10 pl-10 gap-8 animate-enter animate-delay-[360ms] overflow-x-auto mask">
             <ShowoffSection title="Writing">
+              <WritingShowoff slug={"surprise-pass"} />
               <WritingShowoff slug={"january-journal"} />
-              <WritingShowoff slug={"procrastination"} />
 
               {/* TODO: Make the dots jump */}
               <Showoff
