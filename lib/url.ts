39574b615d7447227a51a79b900256b0be65f140
Joey Van Lierop <joey.vanlierop@me.com>
2024-07-07 18:56:56 -0600

    Fix local build issue

--- lib/url.ts
+++ lib/url.ts
@@ -1,5 +1,5 @@
 export const getUrl = () => {
-  if (process.env.VERCEL_ENV === undefined) return "localhost:3000";
+  if (process.env.VERCEL_ENV === undefined) return "http://localhost:3000";
   if (process.env.VERCEL_ENV === "production")
     return "https://joeyvanlierop.com";
   return `https://${process.env.VERCEL_URL}`;
