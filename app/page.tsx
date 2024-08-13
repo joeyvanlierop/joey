28a7080a28ac6cd61abeaf7bb0ed5170701ec92e
Joey Van Lierop <joey.vanlierop@me.com>
2024-08-13 16:38:01 -0600

    Showoff Olympic Shoes

--- app/page.tsx
+++ app/page.tsx
@@ -49,8 +49,8 @@ export default async function Home() {
           {/* Pillars */}
           <section className="flex mb-8 -mx-10 pl-10 gap-8 animate-enter animate-delay-[360ms] overflow-x-auto mask">
             <ShowoffSection title="Writing">
+              <WritingShowoff slug={"olympic-shoes"} />
               <WritingShowoff slug={"a-roll-of-winter"} />
-              <WritingShowoff slug={"january-journal"} />
 
               {/* TODO: Make the dots jump */}
               <Showoff
