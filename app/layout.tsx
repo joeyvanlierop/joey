a8b50b9a7e86259ac0056d80e4d111dcaeed712f
Joey Van Lierop <joey.vanlierop@me.com>
2024-11-21 14:01:26 -0700

    Add rss feed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
-import "../styles/globals.css";
-import "../styles/blockquote.css";
-import "../styles/theme.css";
-import "../styles/scroll.css";
 import "../styles/animations.css";
+import "../styles/blockquote.css";
+import "../styles/globals.css";
 import "../styles/plotly.css";
 import "../styles/react-medium-image-zoom.css";
+import "../styles/scroll.css";
+import "../styles/theme.css";
 
 import { Footer } from "@components/footer";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import Providers from "./providers";
-import Head from "next/head";
 
 const inter = localFont({
   src: "../public/fonts/Inter-roman.latin.var.woff2",
@@ -43,6 +42,13 @@ const sohne = localFont({
 
 export const metadata: Metadata = {
   authors: [{ name: "Joey Van Lierop" }, { name: "Joseph Van Lierop" }],
+  alternates: {
+    types: {
+      "application/rss+xml": "/writing/feed.xml",
+      "application/rss+xml ": "/writing/atom.xml", // Added a space after application/rss+xml to bypass the duplicate key, review in the future
+      "application/rss+json": "/writing/feed.json",
+    }
+  }
 };
 
 export const viewport: Viewport = {
@@ -56,9 +62,6 @@ export default function RootLayout({ children }) {
       className={`bg-mono-1 ${inter.variable} ${newsreader.variable} ${sohne.variable}`}
       suppressHydrationWarning
     >
-      <Head>
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </Head>
       <body className={"font-sans text-text h-full relative"}>
         <Providers>
           <main className="md:py-32 pb-24 min-h-[100dvh] relative">
--- app/page.tsx
+++ app/page.tsx
@@ -22,145 +22,147 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Column className="flex flex-col relative leading-7">
-        <article className="prose-invert prose-headings:font-header flex flex-col gap-11">
-          {/* Introduction */}
-          <section className="flex flex-col gap-6">
-            <h1 className="font-medium leading-6">Joey Van Lierop</h1>
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <Column className="flex flex-col relative leading-7">
+          <article className="prose-invert prose-headings:font-header flex flex-col gap-11">
+            {/* Introduction */}
+            <section className="flex flex-col gap-6">
+              <h1 className="font-medium leading-6">Joey Van Lierop</h1>
 
-            <p className="animate-enter animate-delay-[120ms]">
-              A bit of my brain. Updated every now and then, but
-              mostly then.
-            </p>
+              <p className="animate-enter animate-delay-[120ms]">
+                A bit of my brain. Updated every now and then, but
+                mostly then.
+              </p>
 
-            <p className="mb-8 animate-enter animate-delay-[240ms]">
-              Studying computer science while developing{" "}
-              <a
-                href="https://cesium.com/blog/2023/08/31/fluid-planet-immerses-young-students-in-the-outdoors/"
-                target="_blank"
-              >
-                Fluid Planet
-              </a>
-              .
-            </p>
-          </section>
+              <p className="mb-8 animate-enter animate-delay-[240ms]">
+                Studying computer science while developing{" "}
+                <a
+                  href="https://cesium.com/blog/2023/08/31/fluid-planet-immerses-young-students-in-the-outdoors/"
+                  target="_blank"
+                >
+                  Fluid Planet
+                </a>
+                .
+              </p>
+            </section>
 
-          {/* Pillars */}
-          <section className="flex mb-8 -mx-10 pl-10 gap-8 animate-enter animate-delay-[360ms] overflow-x-auto mask">
-            <ShowoffSection title="Writing">
-              <WritingShowoff slug={"olympic-shoes"} />
-              <WritingShowoff slug={"a-roll-of-winter"} />
+            {/* Pillars */}
+            <section className="flex mb-8 -mx-10 pl-10 gap-8 animate-enter animate-delay-[360ms] overflow-x-auto mask">
+              <ShowoffSection title="Writing">
+                <WritingShowoff slug={"olympic-shoes"} />
+                <WritingShowoff slug={"a-roll-of-winter"} />
 
-              {/* TODO: Make the dots jump */}
-              <Showoff
-                title="...more"
-                description={"A collection of my infrequent thoughts."}
-                href={"/writing/"}
-              />
-            </ShowoffSection>
-            <ShowoffSection title="Projects">
-              <Showoff
-                title="GoLF"
-                description={"A language and compiler for a subset of Go."}
-                href={"https://github.com/joeyvanlierop/golf"}
-                external
-              />
-              <Showoff
-                title="Ship It"
-                description={"One of the game jams of all time."}
-                href={"https://github.com/joeyvanlierop/ld-51"}
-                external
-              />
-            </ShowoffSection>
-            <ShowoffSection title="Videos">
-              <Showoff
-                title="Fairy Meadows"
-                description={"Backcountry"}
-                href={"https://youtu.be/iSd6qFyObP4?si=iUD6VAX_3P88T9Gm"}
-                external
-              />
-              <Showoff
-                title="Lake Louise"
-                description={"Slackcountry"}
-                href={"https://youtu.be/mALdgkO1vtQ?si=B01csEgAR5vvg-RB"}
-                external
-              />
-              <Showoff
-                title="Improvement"
-                description={"Getting stronger"}
-                href={"https://youtu.be/66ND2IStaTI?si=2Xgp9YKyNLJER4z6"}
-                external
-              />
-            </ShowoffSection>
-          </section>
+                {/* TODO: Make the dots jump */}
+                <Showoff
+                  title="...more"
+                  description={"A collection of my infrequent thoughts."}
+                  href={"/writing/"}
+                />
+              </ShowoffSection>
+              <ShowoffSection title="Projects">
+                <Showoff
+                  title="GoLF"
+                  description={"A language and compiler for a subset of Go."}
+                  href={"https://github.com/joeyvanlierop/golf"}
+                  external
+                />
+                <Showoff
+                  title="Ship It"
+                  description={"One of the game jams of all time."}
+                  href={"https://github.com/joeyvanlierop/ld-51"}
+                  external
+                />
+              </ShowoffSection>
+              <ShowoffSection title="Videos">
+                <Showoff
+                  title="Fairy Meadows"
+                  description={"Backcountry"}
+                  href={"https://youtu.be/iSd6qFyObP4?si=iUD6VAX_3P88T9Gm"}
+                  external
+                />
+                <Showoff
+                  title="Lake Louise"
+                  description={"Slackcountry"}
+                  href={"https://youtu.be/mALdgkO1vtQ?si=B01csEgAR5vvg-RB"}
+                  external
+                />
+                <Showoff
+                  title="Improvement"
+                  description={"Getting stronger"}
+                  href={"https://youtu.be/66ND2IStaTI?si=2Xgp9YKyNLJER4z6"}
+                  external
+                />
+              </ShowoffSection>
+            </section>
 
-          {/* Current events */}
-          <section className="flex flex-col gap-8">
-            <h2 className="animate-enter animate-delay-[480ms] font-medium">
-              Today
-            </h2>
+            {/* Current events */}
+            <section className="flex flex-col gap-8">
+              <h2 className="animate-enter animate-delay-[480ms] font-medium">
+                Today
+              </h2>
 
-            <p className="animate-enter animate-delay-[600ms]">
-              Almost done school (ish). Becoming an expert in Greek and Roman studies somehow. Getting stoked for ski season soon, we're due for lots of snow.
-            </p>
+              <p className="animate-enter animate-delay-[600ms]">
+                Almost done school (ish). Becoming an expert in Greek and Roman studies somehow. Getting stoked for ski season soon, we're due for lots of snow.
+              </p>
 
-            <p className="animate-enter animate-delay-[600ms]">
-              Last semester, the{" "}
-              <a
-                target="_blank"
-                href="https://web.archive.org/web/20220929210033/https://contacts.ucalgary.ca/info/cpsc/courses/w19/CPSC525"
-              >
-                (ethical) hacking course with Ryan Henry
-              </a>{" "}
-              showed me how evil code can be if you are clever enough.{" "}
-              <a
-                target="_blank"
-                href="https://web.archive.org/web/20231202114757/https://hist.ucalgary.ca/manageprofile/courses/f23/HTST200"
-              >
-                <em className="fancy">Events and Ideas that Shook the World</em>{" "}
-                with Paul Chastko
-              </a>{" "}
-              was a fun chronological exploration of revolution. I will say that
-              nothing can quite beat{" "}
-              <a
-                target="_blank"
-                href="https://web.archive.org/web/20200702034239/https://scpa.ucalgary.ca/manageprofile/courses/w21/MUSI306.4"
-              >
-                <em className="fancy">The </em>
-                <Nook popup="🎶">
-                  <em className="fancy">Music </em>
-                </Nook>
-                <em className="fancy">of </em>
-                <Nook popup="🎸">
-                  <em className="fancy">Led Zeppelin </em>
-                </Nook>
-                with Ralph Maier.
-              </a>
-            </p>
+              <p className="animate-enter animate-delay-[600ms]">
+                Last semester, the{" "}
+                <a
+                  target="_blank"
+                  href="https://web.archive.org/web/20220929210033/https://contacts.ucalgary.ca/info/cpsc/courses/w19/CPSC525"
+                >
+                  (ethical) hacking course with Ryan Henry
+                </a>{" "}
+                showed me how evil code can be if you are clever enough.{" "}
+                <a
+                  target="_blank"
+                  href="https://web.archive.org/web/20231202114757/https://hist.ucalgary.ca/manageprofile/courses/f23/HTST200"
+                >
+                  <em className="fancy">Events and Ideas that Shook the World</em>{" "}
+                  with Paul Chastko
+                </a>{" "}
+                was a fun chronological exploration of revolution. I will say that
+                nothing can quite beat{" "}
+                <a
+                  target="_blank"
+                  href="https://web.archive.org/web/20200702034239/https://scpa.ucalgary.ca/manageprofile/courses/w21/MUSI306.4"
+                >
+                  <em className="fancy">The </em>
+                  <Nook popup="🎶">
+                    <em className="fancy">Music </em>
+                  </Nook>
+                  <em className="fancy">of </em>
+                  <Nook popup="🎸">
+                    <em className="fancy">Led Zeppelin </em>
+                  </Nook>
+                  with Ralph Maier.
+                </a>
+              </p>
 
-            <p className="animate-enter animate-delay-[740ms]">
-              Borrowing design ideas from from{" "}
-              <a href={"https://paco.me/"} target={"_blank"}>
-                Paco
-              </a>
-              ,{" "}
-              <a href={"https://linear.app/"} target={"_blank"}>
-                Linear
-              </a>
-              , and{" "}
-              <a href={"https://www.notion.so/"} target={"_blank"}>
-                Notion
-              </a>
-              . Learning how to make things detailed, elegant, and
-              blazingly-fast™. Trying to understand how to combine it all into
-              a unparalleled, unified experience. Buzzword buzzword buzzword.
-              Buzzword.
-            </p>
-          </section>
-        </article>
-      </Column>
-    </div>
+              <p className="animate-enter animate-delay-[740ms]">
+                Borrowing design ideas from from{" "}
+                <a href={"https://paco.me/"} target={"_blank"}>
+                  Paco
+                </a>
+                ,{" "}
+                <a href={"https://linear.app/"} target={"_blank"}>
+                  Linear
+                </a>
+                , and{" "}
+                <a href={"https://www.notion.so/"} target={"_blank"}>
+                  Notion
+                </a>
+                . Learning how to make things detailed, elegant, and
+                blazingly-fast™. Trying to understand how to combine it all into
+                a unparalleled, unified experience. Buzzword buzzword buzzword.
+                Buzzword.
+              </p>
+            </section>
+          </article>
+        </Column>
+      </div>
+    </>
   );
 }
 
