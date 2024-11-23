91975d4a0b666e4f5abd40b7701540545f0ec85c
Joey Van Lierop <joey.vanlierop@me.com>
2024-11-23 13:39:04 -0700

    Clean up some january journal styles

--- components/journal.tsx
+++ components/journal.tsx
@@ -30,7 +30,7 @@ function Entry(props: { time: string; children: React.ReactNode }) {
       >
         {props.time}
       </time>
-      <div className="flex flex-col gap-4 [&>p]:m-0 [&>img]:mt-2 [&>img]:mb-0">
+      <div className="flex flex-col gap-4 [&>p]:m-0 [&_img]:mt-2 [&_img]:mb-0">
         {props.children}
       </div>
     </div>
