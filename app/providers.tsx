2aa593a767e77c9f085609cb59576a10dcabacd0
Joey Van Lierop <joey.vanlierop@me.com>
2024-11-23 13:38:54 -0700

    Fix a weird mdx provider issue

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,5 +11,9 @@ import { MDXProvider } from "@mdx-js/react";
 const components = { Definition, Center, Spacer, Image, Stack, Stars };
 
 export default function Providers({ children }) {
+  // TODO: Review if there is a better fix for this
+  // https://github.com/contentlayerdev/contentlayer/issues/456
+  // For now we just ignore the typescript error since it isnt actually an issue (?)
+  //@ts-ignore
   return <MDXProvider components={components}>{children}</MDXProvider>;
 }
