7ba9ea1c29d3fdabce96ad641cb3a0cc3ac449fa
Joey Van Lierop <joey.vanlierop@me.com>
2024-12-05 23:25:06 -0700

    Update mood

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,14 @@ export function Footer() {
       <div className="flex justify-between items-center w-full max-w-full md:max-w-[640px] px-10 md:px-0">
         <Mood
           current={{
-            message: "Shmovember",
-            date: "2024-11-14",
+            message: "Melancholic",
+            date: "2024-12-05",
           }}
           history={[
+            {
+              message: "Shmovember",
+              date: "2024-11-14",
+            },
             {
               message: "Big month",
               date: "2024-10-07",
