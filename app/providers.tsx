232139e8af75246f66f246d3c7663655c1a5754c
Joey Van Lierop <joey.vanlierop@me.com>
2024-12-29 20:16:06 -0700

    Update dependencies

--- app/providers.tsx
+++ app/providers.tsx
@@ -9,9 +9,5 @@ import { MDXProvider } from "@mdx-js/react";
 const components = { Definition, Center, Spacer, Stars };
 
 export default function Providers({ children }) {
-  // TODO: Review if there is a better fix for this
-  // https://github.com/contentlayerdev/contentlayer/issues/456
-  // For now we just ignore the typescript error since it isnt actually an issue (?)
-  //@ts-ignore
   return <MDXProvider components={components}>{children}</MDXProvider>;
 }
